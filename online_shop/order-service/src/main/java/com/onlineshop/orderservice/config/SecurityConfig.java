41955c82d7bb15d50772e44bf8d143d14d030284
Aliaksandr Vidzevich <aliaksandr.vidzevich@innowise.com>
2024-06-26 11:37:04 +0300

    remove csrf.disable()

--- online_shop/order-service/src/main/java/com/onlineshop/orderservice/config/SecurityConfig.java
+++ online_shop/order-service/src/main/java/com/onlineshop/orderservice/config/SecurityConfig.java
@@ -34,7 +34,6 @@ public class SecurityConfig {
     @Order(1)
     @Bean
     public SecurityFilterChain clientFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
         http.authorizeHttpRequests(auth -> auth
                 .anyRequest().permitAll());
         http.oauth2ResourceServer(
